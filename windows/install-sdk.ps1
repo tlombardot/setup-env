1bfc2734957423e018fcffa8ceaedc7d19cd8eff
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-22 15:15:30 +0200

    fix: openjdk for windows

--- windows/install-sdk.ps1
+++ windows/install-sdk.ps1
@@ -1,5 +1,5 @@
 # Installer OpenJDK (JDK) : https://openjdk.org/
-choco install temurinjdk24 -y
+choco install openjdk -y
 
 # Installer Node.js (et npm) : https://nodejs.org/en
 choco install nodejs-lts -y
