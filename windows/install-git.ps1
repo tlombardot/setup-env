62fdf765238ba7b34b8813b5fb0279122d7522b0
Yoan Thirion <yoan.thirion@gmail.com>
2025-09-23 11:49:47 +0200

    fix: git script for windows

--- windows/install-git.ps1
+++ windows/install-git.ps1
@@ -1,10 +1,3 @@
-# Installer Chocolatey si ce n'est pas déjà fait
-if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-}
-
 # Installer Git
 choco install git -y
 
